adcbd0c8c0e5ba803af69831494dc34a5d398c27
Ryan Sprague <rj.sprague@gmail.com>
2024-10-27 23:46:20 +0900

    Fix merge conflict

--- src/components/ProfileComponent.tsx
+++ src/components/ProfileComponent.tsx
@@ -12,107 +12,107 @@ import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
 export default function ProfileComponent() {
-    const profile = useProfileStore((state) => state.profile);
-    const router = useRouter();
-    const updateProfile = useProfileStore((state) => state.updateProfile);
-    const [fireworksApiKey, setFireworksApiKey] = useState(
-        profile.fireworks_api_key
-    );
-    const [openaiApiKey, setOpenaiApiKey] = useState(profile.openai_api_key);
-    const [useCredits, setUseCredits] = useState(profile.useCredits);
-    const [showCreditsSection, setShowCreditsSection] = useState(true); // State to control visibility
-    const addCredits = useProfileStore((state) => state.addCredits);
-    const addPayment = usePaymentsStore((state) => state.addPayment);
-    const deleteAccount = useProfileStore((state) => state.deleteAccount);
-    const clearAuthDetails = useAuthStore((s) => s.clearAuthDetails);
-    const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const profile = useProfileStore((state) => state.profile);
+  const router = useRouter();
+  const updateProfile = useProfileStore((state) => state.updateProfile);
+  const [fireworksApiKey, setFireworksApiKey] = useState(
+    profile.fireworks_api_key
+  );
+  const [openaiApiKey, setOpenaiApiKey] = useState(profile.openai_api_key);
+  const [useCredits, setUseCredits] = useState(profile.useCredits);
+  const [showCreditsSection, setShowCreditsSection] = useState(true); // State to control visibility
+  const addCredits = useProfileStore((state) => state.addCredits);
+  const addPayment = usePaymentsStore((state) => state.addPayment);
+  const deleteAccount = useProfileStore((state) => state.deleteAccount);
+  const clearAuthDetails = useAuthStore((s) => s.clearAuthDetails);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
 
-    useEffect(() => {
-        const handleMessageFromRN = async (event: MessageEvent) => {
-            // Process the message sent from React Native
-            const message = event.data;
-            if (message?.type === "IAP_SUCCESS") {
-                await addPayment({
-                    id: message.message,
-                    amount: message.amount,
-                    status: "succeeded",
-                    mode: "iap",
-                    platform: message.platform,
-                    productId: message.productId,
-                    currency: message.currency,
-                });
-                await addCredits(10000);
-            }
-        };
-
-        // Listen for messages from the RN WebView
-        window.addEventListener("message", handleMessageFromRN);
-
-        return () => {
-            window.removeEventListener("message", handleMessageFromRN);
-        };
-    }, [addCredits, addPayment]);
-
-    useEffect(() => {
-        setFireworksApiKey(profile.fireworks_api_key);
-        setOpenaiApiKey(profile.openai_api_key);
-    }, [profile.fireworks_api_key, profile.openai_api_key]);
-
-    useEffect(() => {
-        // Conditionally hide the credits purchase section if in a React Native WebView on iOS
-        setShowCreditsSection(!isIOSReactNativeWebView());
-    }, []);
-
-    const handleApiKeyChange = async () => {
-        if (
-            fireworksApiKey !== profile.fireworks_api_key ||
-            openaiApiKey !== profile.openai_api_key
-        ) {
-            try {
-                await updateProfile({
-                    fireworks_api_key: fireworksApiKey,
-                    openai_api_key: openaiApiKey,
-                });
-                console.log("API keys updated successfully!");
-            } catch (error) {
-                console.error("Error updating API keys:", error);
-            }
-        }
+  useEffect(() => {
+    const handleMessageFromRN = async (event: MessageEvent) => {
+      // Process the message sent from React Native
+      const message = event.data;
+      if (message?.type === "IAP_SUCCESS") {
+        await addPayment({
+          id: message.message,
+          amount: message.amount,
+          status: "succeeded",
+          mode: "iap",
+          platform: message.platform,
+          productId: message.productId,
+          currency: message.currency,
+        });
+        await addCredits(10000);
+      }
     };
 
-    const handleCreditsChange = async (
-        e: React.ChangeEvent<HTMLSelectElement>
-    ) => {
-        setUseCredits(e.target.value === "credits");
-        await updateProfile({ useCredits: e.target.value == "credits" });
+    // Listen for messages from the RN WebView
+    window.addEventListener("message", handleMessageFromRN);
+
+    return () => {
+      window.removeEventListener("message", handleMessageFromRN);
     };
+  }, [addCredits, addPayment]);
 
-    const handleBuyClick = useCallback(() => {
-        if (showCreditsSection) {
-            window.location.href = "/payment-attempt";
-        } else {
-            window.ReactNativeWebView?.postMessage("INIT_IAP");
-        }
-    }, [showCreditsSection]);
+  useEffect(() => {
+    setFireworksApiKey(profile.fireworks_api_key);
+    setOpenaiApiKey(profile.openai_api_key);
+  }, [profile.fireworks_api_key, profile.openai_api_key]);
 
-    const handleDeleteClick = () => {
-        setShowDeleteModal(true);
-    };
+  useEffect(() => {
+    // Conditionally hide the credits purchase section if in a React Native WebView on iOS
+    setShowCreditsSection(!isIOSReactNativeWebView());
+  }, []);
 
-    const onDeleteConfirm = useCallback(async () => {
-        setShowDeleteModal(false);
-        try {
-            await deleteAccount();
-            await signOut(auth);
-            clearAuthDetails();
-            toast.success("Account deleted successfully.");
-            router.replace("/");
-        } catch (error) {
-            console.error("Error on deletion of account:", error);
-        }
-    }, [deleteAccount, clearAuthDetails, router]);
+  const handleApiKeyChange = async () => {
+    if (
+      fireworksApiKey !== profile.fireworks_api_key ||
+      openaiApiKey !== profile.openai_api_key
+    ) {
+      try {
+        await updateProfile({
+          fireworks_api_key: fireworksApiKey,
+          openai_api_key: openaiApiKey,
+        });
+        console.log("API keys updated successfully!");
+      } catch (error) {
+        console.error("Error updating API keys:", error);
+      }
+    }
+  };
 
-    const areApiKeysAvailable = fireworksApiKey && openaiApiKey;
+  const handleCreditsChange = async (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setUseCredits(e.target.value === "credits");
+    await updateProfile({ useCredits: e.target.value == "credits" });
+  };
+
+  const handleBuyClick = useCallback(() => {
+    if (showCreditsSection) {
+      window.location.href = "/payment-attempt";
+    } else {
+      window.ReactNativeWebView?.postMessage("INIT_IAP");
+    }
+  }, [showCreditsSection]);
+
+  const handleDeleteClick = () => {
+    setShowDeleteModal(true);
+  };
+
+  const onDeleteConfirm = useCallback(async () => {
+    setShowDeleteModal(false);
+    try {
+      await deleteAccount();
+      await signOut(auth);
+      clearAuthDetails();
+      toast.success("Account deleted successfully.");
+      router.replace("/");
+    } catch (error) {
+      console.error("Error on deletion of account:", error);
+    }
+  }, [deleteAccount, clearAuthDetails, router]);
+
+  const areApiKeysAvailable = fireworksApiKey && openaiApiKey;
 
   return (
     <div className="flex flex-col gap-4">
@@ -173,23 +173,39 @@ export default function ProfileComponent() {
         </div>
       )}
 
-            <div className="flex flex-col">
-                <label htmlFor="setting-label-key" className="text-sm font-medium text-primary">
-                    Settings:
-                </label>
-                <button
-                    className="btn-primary bg-[#e32012] self-start rounded-md hover:bg-[#e32012]/30 w-fit"
-                    onClick={handleDeleteClick}
-                >
-                    Delete Account
-                </button>
-            </div>
+      <div className="flex flex-col px-5 py-3 gap-3 border border-gray-500 rounded-md">
+        <label htmlFor="setting-lable-key" className="text-sm font-medium">
+          Settings:
+        </label>
+        <button
+          className="btn-primary bg-[#e32012] self-start rounded-md hover:bg-[#e32012]/30"
+          onClick={handleDeleteClick}
+        >
+          Delete Account
+        </button>
+      </div>
 
-            <DeleteConfirmModal
-                showDeleteModal={showDeleteModal}
-                onHideModal={() => setShowDeleteModal(false)}
-                onDeleteConfirm={onDeleteConfirm}
-            />
-        </div>
-    );
+      <div className="flex items-center px-5 py-3 gap-3 border border-gray-500 rounded-md">
+        <label htmlFor="toggle-use-credits" className="text-sm font-medium">
+          Use:
+        </label>
+        <select
+          id="toggle-use-credits"
+          value={useCredits ? "credits" : "apikeys"}
+          onChange={handleCreditsChange}
+          className="border border-gray-300 rounded-md px-3 py-2 h-10"
+          disabled={!areApiKeysAvailable}
+        >
+          <option value="credits">Credits</option>
+          {areApiKeysAvailable && <option value="apikeys">API Keys</option>}
+        </select>
+      </div>
+
+      <DeleteConfirmModal
+        showDeleteModal={showDeleteModal}
+        onHideModal={() => setShowDeleteModal(false)}
+        onDeleteConfirm={onDeleteConfirm}
+      />
+    </div>
+  );
 }
